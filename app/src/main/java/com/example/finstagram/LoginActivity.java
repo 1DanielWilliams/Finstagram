cd0ea43f7cc390ac9baf3856ecd9cb593b388976
1DanielWilliams <84046935+1DanielWilliams@users.noreply.github.com>
2022-06-13 16:13:18 -0700

    Added logout functionality

--- app/src/main/java/com/example/finstagram/LoginActivity.java
+++ app/src/main/java/com/example/finstagram/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -71,5 +72,11 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(i);
         // Makes it so that a user cannot simply press the back button after logging in
         finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.login_menu, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/finstagram/MainActivity.java
+++ app/src/main/java/com/example/finstagram/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.finstagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuLogout:
+                ParseUser.logOut();
+                Intent i = new Intent(this, LoginActivity.class);
+                startActivity(i);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
