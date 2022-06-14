72ecbd612fb3d5b9c3baa5f59c4359c13c732ed7
1DanielWilliams <84046935+1DanielWilliams@users.noreply.github.com>
2022-06-14 14:56:42 -0700

    added functionality for saving post to backend

--- app/src/main/java/com/example/finstagram/ParseApplication.java
+++ app/src/main/java/com/example/finstagram/ParseApplication.java
@@ -3,6 +3,7 @@ package com.example.finstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public  class ParseApplication extends Application {
 
@@ -11,7 +12,7 @@ public  class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("PrizTK5goSwWNSCu1u0O11OCiHHB5JVlWjIFfakC")
                 .clientKey("4V2jAjQL3Aw9SIaQJ07iMLO0MFKqi93dePmFkGii")
